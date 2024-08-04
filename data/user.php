24976a56f249fa3102f66d5203f00750e17c7222
mjperez2704 <misaeladrian20@hotmail.com>
2024-08-04 12:12:25 -0600

    Revert "Primer cargar de archivos"
    This reverts commit 6b176f82fb8e92aa7c54e1978a09a2725e3933af.

--- data/user.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$user=json_decode(file_get_contents('php://input'));  //get user from 
-	if($user->mail=='admin@mega-solutions.com.mx' && $user->pass=='admin') 
-		session_start();
-		$_SESSION['uid']=uniqid('ang_');
-		print $_SESSION['uid'];
-?>
